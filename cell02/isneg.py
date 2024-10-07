2dbe9e34c890e19f1afdf84b4754075e2027d451
Tutorji <kanachai1955p@gmail.com>
2024-10-07 15:31:34 +0700

    Add files via upload

---
+++ cell02/isneg.py
@@ -0,0 +1,8 @@
+Is=input("number")
+Is=float(Is)
+if Is == 0:
+    print("this number is both positive and negative")
+if Is  <0:
+    print("Number given by you is Negative")
+if Is >0:   
+     print("Number given by you is Positive")   
