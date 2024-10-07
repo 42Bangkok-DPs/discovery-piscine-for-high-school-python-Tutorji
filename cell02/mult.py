f347127b62514bf3b9a03d04e478e2a981c27188
Tutorji <kanachai1955p@gmail.com>
2024-10-07 16:28:23 +0700

    Add files via upload

---
+++ cell02/mult.py
@@ -0,0 +1,10 @@
+mt=int(input("first number"))
+rt=int(input("second number"))
+my= mt * rt
+print(my)
+if my == 0:
+    print("this number is both positive and negative")
+if my  <0:
+    print("Number given by you is Negative")
+if my >0:   
+     print("Number given by you is Positive")   
