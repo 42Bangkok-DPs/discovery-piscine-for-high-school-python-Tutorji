4d788b70239308fc54746cb611a81090d82d17d4
Tutorji <kanachai1955p@gmail.com>
2024-10-08 13:09:50 +0700

    Add files via upload

---
+++ cell03/i_got_that.py
@@ -0,0 +1,9 @@
+i = input("what you gotta say?")
+hg= input("I got this anything else?")
+stop="STOP"
+
+while hg !=stop:
+    hg= input("I got this anything else?")
+    if hg == stop  :
+        break
+
