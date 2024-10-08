65ee93a1b10bb0e912d19d536a335b17b6a051bc
Tutorji <kanachai1955p@gmail.com>
2024-10-08 15:31:56 +0700

    Add files via upload

---
+++ cell04/ex4.1/age.py
@@ -0,0 +1,10 @@
+age=input("please tell me your age")
+print("yoou are currenty " + age + " years old.")
+age=int(age)
+t = age + 10
+tw = age + 20
+tr = age + 30
+
+print("in 10 years,you'll be "+str(t)+"years olds")
+print("in 20 years,you'll be "+str(tw)+"years olds")
+print("in 30 years,you'll be "+str(tr)+"years olds")
---
+++ cell04/ex4/upcase_it.py
@@ -0,0 +1,2 @@
+t=input("give me word")
+print (t.upper())
